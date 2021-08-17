5f7bf2173364bf152241ad9067b109718e48ab1c
Ilya Dzyuin <86766397+ilua-dz@users.noreply.github.com>
2021-08-18 03:07:10 +0400

    fix: input string name error

--- src/index.js
+++ src/index.js
@@ -39,40 +39,40 @@ const MORSE_TABLE = {
 
 function decode(expr) {
 
-//declare Morse to Bin conformity Object
-let morseBin = {};
-for (let i in MORSE_TABLE) morseBin[i] = MORSE_TABLE[i];
-for (let i in morseBin) {
-  morseBin[i] = i.split('').reduce((binChar, x) => {
-    if (x === '-') binChar.push('11')
-    if (x === '.') binChar.push('10')
-    return binChar;
-  }, []);
-  if (morseBin[i].length < 5)
-    while (morseBin[i].length < 5) morseBin[i].unshift('00');
-  morseBin[i] = morseBin[i].join('');
-}
-
-//declare Bin to Readable conformity Object
-let binReadable = {};
-for (let i in morseBin) {
-  binReadable[morseBin[i]] = MORSE_TABLE[i]
-}
-binReadable["**********"] = ' ';
-
-//convert input to array
-let arr = [];
-for (i = 10; i < a.length+1; i += 10) arr.push(a.slice(i - 10, i));
-
-//construct output array
-for (let key in binReadable) {
-  for (i=0;i<arr.length;i++) {
-    if (arr[i] === key) arr[i] = binReadable[key];
+  //declare Morse to Bin conformity Object
+  let morseBin = {};
+  for (let i in MORSE_TABLE) morseBin[i] = MORSE_TABLE[i];
+  for (let i in morseBin) {
+    morseBin[i] = i.split('').reduce((binChar, x) => {
+      if (x === '-') binChar.push('11')
+      if (x === '.') binChar.push('10')
+      return binChar;
+    }, []);
+    if (morseBin[i].length < 5)
+      while (morseBin[i].length < 5) morseBin[i].unshift('00');
+    morseBin[i] = morseBin[i].join('');
   }
-}
 
-//construct output string
-return arr.join(''); 
+  //declare Bin to Readable conformity Object
+  let binReadable = {};
+  for (let i in morseBin) {
+    binReadable[morseBin[i]] = MORSE_TABLE[i]
+  }
+  binReadable["**********"] = ' ';
+
+  //convert input to array
+  let arr = [];
+  for (i = 10; i < expr.length + 1; i += 10) arr.push(expr.slice(i - 10, i));
+
+  //construct output array
+  for (let key in binReadable) {
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] === key) arr[i] = binReadable[key];
+    }
+  }
+
+  //construct output string
+  return arr.join('');
 }
 
 module.exports = {
